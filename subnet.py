28b35969d28a80e59787539f98d2b30ea5cbcf0d
hammamiomar <oh524@nyu.edu>
2024-03-29 04:01:58 -0400

    done

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 37818
+
+#problem 1
+p1Network = '86.137.73.0'
+p1Broadcast = '86.136.73.255'
+p1First = '86.136.73.1'
+p1Last =  '86.136.73.254'
+
+#problem 2
+p2Network = '253.121.35.96'
+p2Broadcast = '253/121/35/111'
+p2First = '253.121.35.97'
+p2Last = '253.121.35.110'
+
+#problem 3
+p3Network = '164.106.252.128'
+p3Broadcast = '164.106.252.255'
+p3First = '164.106.252.129'
+p3Last = '164.106.252.254'
+
+#problem 4
+p4Network = '75.91.214.192'
+p4Broadcast = '75.91.214.199'
+p4First = '75.91.214.193'
+p4Last = '75.91.214.198'
+
+#problem 5
+p5Network = '242.77.176.192'
+p5Broadcast = '242.77.176.255'
+p5First = '242.77.176.193'
+p5Last = '242.77.176.254'
+
+#problem 6
+p6Network = '153.62.138.0'
+p6Broadcast = '153.62.138.255'
+p6First = '153.62.138.1'
+p6Last =  '153.62.138.254'
+
+#problem 7
+p7Network = '64.47.100.64'
+p7Broadcast = '64.47.100.95'
+p7First = '64.47.100.65'
+p7Last = '64.47.100.94'
+
+#problem 8
+p8Network = '231.32.61.0'
+p8Broadcast = '231.32.61.127'
+p8First = '231.32.61.1'
+p8Last = '231.32.61.126'
+
+#problem 9
+p9Network = '142.17.23.144'
+p9Broadcast = '142.17.23.159'
+p9First = '142.17.23.145'
+p9Last = '142.17.23.158'
+
+#problem 10
+p10Network = '53.2.241.192'
+p10Broadcast = '53.2.241.255'
+p10First = '53.2.241.193'
+p10Last = '53.2.241.254'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
